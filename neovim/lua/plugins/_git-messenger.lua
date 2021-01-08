2cf828bd1659cdfb33dc28027d186b2b9dc6b80d
Thiago Talma <thiago@thiagomt.com>
2021-01-07 23:18:41 -0300

    ajuste

--- neovim/lua/plugins/_git-messenger.lua
+++
@@ -1,6 +0,0 @@
-local g = vim.g
-local U = require 'utils'
-
-g.git_messenger_no_default_mappings = true
-
-U.map("n", "gm", ":GitMessenger<CR>")
--- neovim/lua/plugins/_term.lua
+++
@@ -1,4 +0,0 @@
-local U = require 'utils'
-
-U.map('n', '<A-i>', '<CMD>lua require"FTerm".toggle()<CR>')
-U.map('t', '<A-i>', '<C-\\><C-n><CMD>lua require"FTerm".toggle()<CR>')
